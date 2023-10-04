4e49a42453462b47494ecce7cf0ca86f49f13a86
Douglass Coronado <dcoronado@funio.com>
2023-10-04 19:18:09 -0400

    add output variables

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "linux_app_list_output"{
+    value = flatten([
+      for app in local.linux_app : [
+        for linuxapps in try(app.linux_app, []) :{
+          name=linuxapps.name
+          resource_group_name=linuxapps.resource_group
+          location=linuxapps.location
+          os_type=linuxapps.os_type
+          sku_name=linuxapps.sku_name
+        }
+     ]
+])
+}
+
